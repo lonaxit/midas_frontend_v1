8487a5991dcb893ebfd4d487c9c0b2b476114af2
lonaxit <lonaxit@outlook.com>
2022-09-06 17:27:37 +0100

    Added a new rule
    Created a new rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		"no-mixed-spaces-and-tabs": 0, // disable rule
+		"no-unused-vars": 'off'
 	},
 };

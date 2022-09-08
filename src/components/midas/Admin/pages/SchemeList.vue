2dd12c6fbef804fcbca428b7f43d354ed7796208
lonaxit <lonaxit@outlook.com>
2022-09-08 12:05:34 +0100

    Update SchemeList.vue
    Added list heading as a component

--- src/components/midas/Admin/pages/SchemeList.vue
+++ src/components/midas/Admin/pages/SchemeList.vue
@@ -1,16 +1,44 @@
 <template>
-
+<div v-show="$store.state.SchemesProduct.schemes.length == 0">
   <MyLoader msg="Please wait..."></MyLoader>
+  </div>
 
 <div class="surface-section scheme-pad"  v-show="$store.state.SchemesProduct.schemes.length != 0">
     <div class="font-medium text-3xl text-900 mb-3">Schemes</div>
     <div class="text-500 mb-5">MIDAS available schemes.</div>
     <ul class="list-none p-0 m-0">
+       
+        <ListHeader>
+        <template v-slot:useInsideUl>
+             
+        <li class="flex align-items-center py-3 px-2 border-top-1 surface-border flex-wrap">
+            <!-- <div class="text-500 w-6 md:w-2 font-medium">Title</div> -->
+            
+            <div class="w-6 md:w-2 flex justify-content-space-evenly list-heading">
+               Name
+            </div>
+          
+            <div class="w-6 md:w-2 flex justify-content-space-evenly list-heading">
+              Action
+            </div>
+        </li>
+        </template>
+        </ListHeader>
+
         <li class="flex align-items-center py-3 px-2 border-top-1 surface-border flex-wrap" v-for="scheme in Productschemes" :key="scheme.id" >
             <!-- <div class="text-500 w-6 md:w-2 font-medium">Title</div> -->
-            <div class="text-900 w-full md:w-8 md:flex-order-0 flex-order-1">{{scheme.name}}</div>
-            <div class="w-6 md:w-2 flex justify-content-end">
+            
+            <div class="w-6 md:w-2 flex justify-content-space-evenly">
+               {{scheme.name}}
+            </div>
+            
+          
+            <div class="w-6 md:w-2 flex justify-content-space-evenly">
                 <router-link  icon="pi pi-pencil" :to="{name:'update-scheme',params:{id:scheme.id}}">Edit</router-link>
+            </div>
+
+             <div class="w-6 md:w-2 flex justify-content-space-evenly">
+                <router-link  icon="pi pi-pencil" :to="{name:'create-product',params:{id:scheme.id}}">Add Product</router-link>
             </div>
         </li>
     </ul>
@@ -20,6 +48,7 @@
 <script>
 
 import MyLoader from '@/components/midas/ReusableComponents/Loader.vue'
+import ListHeader from '@/components/midas/ReusableComponents/Listheading.vue'
 export default {
 data(){
     return{
@@ -28,7 +57,8 @@ data(){
     }
 },
 components:{
-    MyLoader
+    MyLoader, 
+    ListHeader
 },
 created(){
     this.$store.dispatch('getSchemes')
@@ -65,4 +95,5 @@ computed: {
     border-radius: 4px;
     
 }
+
 </style>

20dfc56778b9a807703bbaa5c333b8c6d79b552e
lonaxit <lonaxit@outlook.com>
2022-09-08 12:06:30 +0100

    Update schemes_product.js
    Refactored code

--- src/store/modules/schemes_product.js
+++ src/store/modules/schemes_product.js
@@ -2,12 +2,16 @@ import axios from 'axios'
 const schemes = {
 
   state: {
-    schemes:[],
+    schemes: [],
+    products:[]
 },
 mutations: {
     LIST_SCHEMES(state, schemesList) {
         state.schemes = schemesList;
-    },
+  },
+  LIST_PRODUCTS(state, productsList) {
+    state.products = productsList;
+},
   },
   getters: {
   
@@ -20,10 +24,41 @@ async getSchemes(context) {
       const res = await axios.get('api/v1/schemes/')
         context.commit('LIST_SCHEMES', res.data)
       }
-      catch (error) {
-          console.log(error)
+    catch(err) {
+      // if (err.response) {
+      //   this.$toast.add({severity: 'error', detail:err.response.data, life: 5000});
+      // } else if (err.request) {
+      //   this.$toast.add({severity: 'error', detail:err.request, life: 5000}); 
+      // }else{
+      //   this.$toast.add({severity: 'error', detail:err.message, life: 5000}); 
+      // }
+      console.log(err)
+          
       }
   },
+
+
+  // get list of products
+  async getProducts(context) {
+
+    try {
+      const res = await axios.get('api/v1/products/')
+        context.commit('LIST_PRODUCTS', res.data)
+      }
+      catch (err) {
+        // if (err.response) {
+        //   this.$toast.add({severity: 'error', detail:err.response.data, life: 5000});
+        // } else if (err.request) {
+        //   this.$toast.add({severity: 'error', detail:err.request, life: 5000}); 
+        // }else{
+        //   this.$toast.add({severity: 'error', detail:err.message, life: 5000}); 
+        // }
+        console.log(err)
+      }
+  }
+
+
+
 },
 
 }

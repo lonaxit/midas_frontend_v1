74fe1e43214da648149d38af8995066a152f483c
lonaxit <lonaxit@outlook.com>
2022-09-21 13:53:29 +0100

    Update NewLoan.vue
    Corrected v-model names

--- src/components/midas/Admin/pages/Loans/NewLoan.vue
+++ src/components/midas/Admin/pages/Loans/NewLoan.vue
@@ -3,49 +3,58 @@
 		<div class="col-12">
             <div class="card  p-fluid">
             <h5>New Loan</h5>
-            <form @submit.prevent="createProduct">
+            <form @submit.prevent="createLoan">
 				<div class="p-fluid formgrid grid">
-					<div class="field col-12 md:col-4">
-						<label for="firstname2">Amount</label>
-						<InputText id="firstname2" type="text"/>
+                    <div class="field col-12 md:col-3">
+						<label for="userid">UserID</label>
+						<InputText id="userid" type="number" v-model="userid"/>
 					</div>
 
-					<div class="field col-12 md:col-4">
-						<label for="lastname2">Approved</label>
-						<InputText id="lastname2" type="text"/>
+					<div class="field col-12 md:col-3">
+						<label for="amount">Requested Amount</label>
+						<InputText id="amount" type="number" v-model="amount"/>
 					</div>
 
-                    	<div class="field col-12 md:col-4">
-						<label for="lastname2">Deduction</label>
-						<InputText id="lastname2" type="text"/>
+					<div class="field col-12 md:col-3">
+						<label for="approved">Approved Amount</label>
+						<InputText id="approved" type="number" v-model="approved"/>
 					</div>
 
-                    <div class="field col-12 md:col-4">
-						<label for="firstname2">Loan Date</label>
-						<InputText id="firstname2" type="text"/>
+                    	<div class="field col-12 md:col-3">
+						<label for="deduction">Deduction</label>
+						<InputText id="deduction" type="number" v-model="deduction"/>
 					</div>
 
-					<div class="field col-12 md:col-4">
-						<label for="lastname2">Start Date</label>
-						<InputText id="lastname2" type="text"/>
+<div class="field col-12 md:col-3">
+						<label for="loandate">Loan Date</label>
+                  
+                       <InputText id="loandate" type="date" v-model="loandate"/>
 					</div>
 
-                    	<div class="field col-12 md:col-4">
-						<label for="lastname2">End Date</label>
-						   
+					<div class="field col-12 md:col-3">
+						<label for="startdate">Start Date</label>
+                         <InputText id="startdate" type="date" v-model="startdate"/>
 					</div>
 
-					<div class="field col-12 md:col-4">
-						<label for="zip">Tenor</label>
-						<InputText id="zip" type="text"/>
+                    <div class="field col-12 md:col-3">
+						<label for="enddate">End Date</label>
+                        <InputText id="enddate" type="date" v-model="enddate"/>
+                        <!-- <date-picker v-model:value="enddate"></date-picker> -->
 					</div>
-					<div class="field col-12 md:col-4">
-						<label for="city">Net Pay</label>
-						<InputText id="city" type="text" />
+
+                   
+
+					<div class="field col-12 md:col-3">
+						<label for="tenor">Tenor</label>
+						<InputText id="tenor" type="number" v-model="tenor"/>
 					</div>
-					<div class="field col-12 md:col-4">
-						<label for="state">Products</label>
-						<Dropdown id="state" v-model="selectedProduct" :options="Products" optionLabel="name" placeholder="Select One"></Dropdown>
+					<div class="field col-12 md:col-6">
+						<label for="netpay">Net Pay</label>
+						<InputText id="netpay" type="number" v-model="netpay"/>
+					</div>
+					<div class="field col-12 md:col-6">
+						<label for="products">Products</label>
+						<Dropdown id="products" v-model="selectedProduct" :options="allPrducts" optionLabel="name" placeholder="Select One"></Dropdown>
 					</div>
 					
 				</div>
@@ -56,101 +65,118 @@
                 </SubmitButton>
                 </form>
 
-			<!-- <div class="card  p-fluid">
-				<h5>Create New Loan</h5>
-				<form @submit.prevent="createProduct">
-                <div class="field">
-					<label for="name1">Name</label>
-					<InputText v-model="name" id="name" type="text" placeholder="Enter Name"/>
-				</div>
-			
-                <div class="field">
-				<label for="address">Description</label>
-				<Textarea v-model="description" id="address" rows="4" placeholder="Create an optional  scheme description "/>
-				</div>
-
-                 <div class="field">
-				<label for="state">Products</label>
-				<Dropdown id="scheme" v-model="selectedScheme" :options="Products" optionLabel="name" placeholder="Select Scheme"></Dropdown>
-				</div>
-
-                 <SubmitButton>
-                    <template v-slot:action>
-                        <button>Create Loan</button>
-                    </template>
-                </SubmitButton>
-            
-                </form>
-              
-			</div> -->
-            
-
+		
         </div>
 		</div>
-        
-      
 	</div>
 </template>
 
 <script>
 import axios from 'axios'
 import SubmitButton from '@/components/midas/ReusableComponents/submitUpdateButton.vue'
+// import DatePicker from '@jobinsjp/vue3-datepicker';
+// import '@jobinsjp/vue3-datepicker/index.css';
+ 
 
 export default {
 
     data(){
         return{
-            name:"",
-            description:"",
+        
             selectedProduct:[],
-            product_scheme:"",
-            date:null
+            product_id:null,
+            enddate:null,
+            amount:null,
+            approved:null,
+            deduction:null,
+            loandate:null,
+            startdate:null,
+            tenor:null,
+            netpay:null,
+            userid:null
         }
     },
     components:{
     SubmitButton,
-
+    // DatePicker
 },
     methods: {
-        createProduct(){
+
+        // formattedDate(datevalue) {
+        //      var date = new Date(datevalue)
+        //     var year = date.toLocaleString("default", { year: "numeric" });
+        //     var month = date.toLocaleString("default", { month: "2-digit" });
+        //     var day = date.toLocaleString("default", { day: "2-digit" });
+
+        //     // Generate yyyy-mm-dd date string
+        //     var formatted_Date = year + "-" + month + "-" + day;
+        //     return formatted_Date 
+        // },
+        createLoan(){
+            // Math.floor(1000000 + Math.random() * 9000000)
+            // console.log(Math.floor(Math.random() * 10000)) + 90000;
             
             // initialize the errors array  
-            
-            if(this.name===""){
-               this.$toast.add({severity: 'error', detail:'Please provide a name', life: 5000});
-            }else if(this.selectedScheme.length==0){
-                   this.$toast.add({severity: 'error', detail:'Please select a scheme', life: 5000});
+            if(this.amount == null || this.amount==0 && this.approved == null || this.approved == 0 && this.deduction == null || this.deduction==0 && this.tenor == null || this.tenor==0 && this.netpay== null || this.netpay==0){
+               this.$toast.add({severity: 'error', detail:'Please provide a valid value', life: 5000});
+            }else if(this.selectedProduct.length==0){
+                   this.$toast.add({severity: 'error', detail:'Please select a product', life: 5000});
+            }else if(this.enddate==null || this.startdate==null || this.loandate==null){
+                   this.$toast.add({severity: 'error', detail:'Please select a valid date', life: 5000});
             }else{
+               
+
                 const formData = {
-                    name:this.name, 
-                    description: this.description,
-                    product_scheme:this.selectedScheme.id
+                 
+                end_date:this.enddate,
+                applied_amount:this.amount,
+                approved_amount:this.approved,
+                monthly_deduction:this.deduction,
+                loan_date:this.loandate,
+                start_date: this.startdate,
+                tenor:this.tenor,
+                net_pay:this.netpay,
+                product:this.selectedProduct.id,
+                owner:this.userid,
+                transaction_code:Math.floor(1000000 + Math.random() * 9000000)
                 }
-                
-                const newProduct = async () =>{
+                const newLoan = async () =>{
                 try{
-                const res = await axios.post('api/v1/products/',formData)
+                const res = await axios.post('api/v1/loans/',formData)
                 this.$toast.add({severity: 'success', detail:'Item Successfully Created', life: 5000});
              
-                this.name = ""
-                this.description = ""
-                this.selectedScheme=""
-                this.product_scheme=""
+                this.enddate=null
+                this.amount=null
+                this.approved=null
+                this.deduction=null
+                this.loandate=null
+                this.startdate=null
+                this.tenor=null
+                this.netpay=null
+                this.product_id=null
+                this.userid=null
 
                 } catch(err){
+                   
                 this.$toast.add({severity: 'error', detail:'Something went wrong', life: 5000});
                 }
                 }
-                newProduct()
+                newLoan()
             }
             
                 
         }
     },
     computed: {
-    Products() {
-    return this.$store.state.SchemesProduct.products;
-  }},
+
+    allPrducts() {
+    return this.$store.getters.allPrducts;
+  },
+  products_loader(){
+    return this.$store.getters.products_loader
+  }
+
+  },
 
     created(){
         this.$store.dispatch('getProducts')

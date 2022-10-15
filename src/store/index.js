99fa416abb0d4029aac4085e5e48ef90a90a80cf
lonaxit <lonaxit@outlook.com>
2022-10-15 11:26:40 +0100

    Update index.js
    imported new modules

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,15 @@ import auth from './modules/auth'
 import SchemesProduct from './modules/schemes_product'
 import Loan from './modules/loans'
 import UserProfile from './modules/users'
+import Deduction from './modules/deductions'
 export default createStore({
 
   modules: {
     auth,
     SchemesProduct,
     Loan,
-    UserProfile
+    UserProfile,
+    Deduction
   }
 
 })

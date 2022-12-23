f5df73d4d69bac671f21c7e92ba4b725ccf0dea7
lonaxit <lonaxit@outlook.com>
2022-12-23 18:00:02 +0100

    Update NewDeductions.vue
    commented out formatDate function

--- src/components/midas/Admin/pages/Loans/NewDeductions.vue
+++ src/components/midas/Admin/pages/Loans/NewDeductions.vue
@@ -136,13 +136,13 @@
 			formatCurrency(value) {
 				return value.toLocaleString('en-US', {style: 'currency', currency: 'NGN'});
 			},
-			formatDate(value) {
-				return value.toLocaleDateString('en-US', {
-					day: '2-digit',
-					month: '2-digit',
-					year: 'numeric',
-				});
-			},
+			// formatDate(value) {
+			// 	return value.toLocaleDateString('en-US', {
+			// 		day: '2-digit',
+			// 		month: '2-digit',
+			// 		year: 'numeric',
+			// 	});
+			// },
           
 		}
 	}

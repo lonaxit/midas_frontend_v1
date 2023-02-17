ddd68b62505085271156b8c633c7085fe13e4723
lonaxit <lonaxit@outlook.com>
2023-02-17 08:08:37 +0100

    removed files
    Deleted files

--- src/components/midas/Admin/Dashboard.vue
+++ src/components/midas/Admin/Dashboard.vue
@@ -159,139 +159,30 @@
 
 <script>
 import EventBus from '@/AppEventBus';
-import ProductService from '@/service/ProductService';
+
 // import axios from 'axios'
 export default {
 	data() {
 		return {
-			myRole: false,
-			username:"",
-			products: null,
-			lineData: {
-				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-				datasets: [
-					{
-						label: 'Revenue',
-						data: [65, 59, 80, 81, 56, 55, 40],
-						fill: false,
-						backgroundColor: '#2f4860',
-						borderColor: '#2f4860',
-						tension: 0.4
-					},
-					{
-						label: 'Sales',
-						data: [28, 48, 40, 19, 86, 27, 90],
-						fill: false,
-						backgroundColor: '#00bb7e',
-						borderColor: '#00bb7e',
-						tension: 0.4
-					}
-				]
-			},
-			items: [
-                {label: 'Add New', icon: 'pi pi-fw pi-plus'},
-                {label: 'Remove', icon: 'pi pi-fw pi-minus'}
-            ],
-			lineOptions: null,
+		
 		}
 	},
-	productService: null,
-	themeChangeListener: null,
-	role:null,
+
 	mounted() {
-		this.productService.getProductsSmall().then(data => this.products = data);
+	
 
-		this.themeChangeListener = (event) => {
-            if (event.dark)
-                this.applyDarkTheme();
-            else
-                this.applyLightTheme();
-        };
-        EventBus.on('change-theme', this.themeChangeListener);
 
-		if (this.isDarkTheme()) {
-            this.applyDarkTheme();
-        }
-        else {
-            this.applyLightTheme();
-        }
+
 
 	
 	},
 	beforeUnmount() {
-        EventBus.off('change-theme', this.themeChangeListener );
+     
     },
 	created() {
-		this.productService = new ProductService();
-	
 	},
 	
 	methods: {
-		
-		formatCurrency(value) {
-			return value.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
-		},
-		isDarkTheme() {
-            return this.$appState.darkTheme === true;
-        },
-		applyLightTheme() {
-			this.lineOptions = {
-				plugins: {
-					legend: {
-						labels: {
-							color: '#495057'
-						}
-					}
-				},
-				scales: {
-					x: {
-						ticks: {
-							color: '#495057'
-						},
-						grid: {
-							color:  '#ebedef',
-						}
-					},
-					y: {
-						ticks: {
-							color: '#495057'
-						},
-						grid: {
-							color:  '#ebedef',
-						}
-					},
-				}
-			};
-		},
-		applyDarkTheme() {
-			this.lineOptions = {
-				plugins: {
-					legend: {
-						labels: {
-							color: '#ebedef'
-						}
-					}
-				},
-				scales: {
-					x: {
-						ticks: {
-							color: '#ebedef'
-						},
-						grid: {
-							color:  'rgba(160, 167, 181, .3)',
-						}
-					},
-					y: {
-						ticks: {
-							color: '#ebedef'
-						},
-						grid: {
-							color:  'rgba(160, 167, 181, .3)',
-						}
-					},
-				}
-			};
-		}
 	}
 }
 </script>

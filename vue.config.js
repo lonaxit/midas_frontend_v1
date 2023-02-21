cf5efb2b53fe2e8770748a79f4d546340f5f99fe
lonaxit <lonaxit@outlook.com>
2023-02-21 11:42:15 +0100

    Update vue.config.js
    Updated production folder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
+
+//   module.exports = {
+// 	publicPath: process.env.NODE_ENV === 'production' ? '/sakai-vue' : '/'
+// }
+
+
+// module.exports = {
+// 	publicPath: process.env.NODE_ENV === 'production'
+// 	  ? '/midas_frontend_v1/'
+// 	  : '/'
+// }
+
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production'
-	  ? '/midas_frontend_v1/'
+	  ? '/dist/'
 	  : '/'
   }
